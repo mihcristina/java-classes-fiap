8e8276a3af102524d93341b9032d15bd764b8e73
mihcristina <contato@michellicristina.com.br>
2022-04-29 15:50:39 -0300

    first commit

---
+++ GulliverProject/src/module-info.java
@@ -0,0 +1,2 @@
+module GulliverProject {
+}
---
+++ TicketPurchase/src/br/com/fiap/ticketpurchase/Passenger.java
@@ -0,0 +1,16 @@
+package br.com.fiap.ticketpurchase;
+
+public class Passenger {
+		private int id;
+		private String name;
+		private String rg;
+		private String email;
+		
+		public void Passenger(int id, String name, String rg, String email) {
+			this.id = id;
+			this.name = name;
+			this.rg = rg;
+			this.email = email;
+		}
+
+}
